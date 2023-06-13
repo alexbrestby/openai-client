1ca6bb9e331ba15c2f323423f0e9b06facd2a78c
Alex Leoniuk <alex.brest.by@gmail.com>
2023-06-13 22:43:56 +0300

    feat: telegram-bot-api add

--- python.py
+++ python.py
@@ -1,27 +1,44 @@
 import os
 import openai
+import asyncio
+import telegram
 from dotenv import load_dotenv
 
 load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.organization = os.getenv('OPENAI_ORGANIZATION_KEY')
+bot_api_key = os.getenv('BOT_API_KEY')
 
-openai.api_key = os.getenv('OPENAI_API')
-openai.organization = os.getenv('OPENAI_ORGANIZATION')
-tg_bot_api = os.getenv('BOT_API')
+async def main():
+    bot = telegram.Bot(bot_api_key)
+    async with bot:
+        print(await bot.get_me())
 
-print(tg_bot_api)
+if __name__ == '__main__':
+    asyncio.run(main())
 
-messages = []
+# images = openai.Image.create(
+#   prompt="A cute baby sea otter",
+#   n=2,
+#   size="1024x1024"
+# )
 
-while True:
-    message = input("User: ")   # Prompt the user for input
-    if message == "quit":
-        break
+# print(images)
 
-    messages.append({"role": "user", "content": message})
-    chat = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
-    reply = chat.choices[0].message.content
-    print(f"ChatGPT: {reply}")
-    messages.append({"role": "assistant", "content": reply})
+# messages = []
+
+# while True:
+#     message = input("User: ")   # Prompt the user for input
+#     if message == "quit":
+#         break
+
+#     messages.append({"role": "user", "content": message})
+#     chat = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         temperature=0.8,
+#         messages=messages
+#     )
+#     reply = chat
+#     # reply = chat.choices[0].message.content
+#     print(f"ChatGPT: {reply}")
+#     messages.append({"role": "assistant", "content": reply})
